1baaed62c9f13f545f2c6cb63110516b759253fb
DIEGO MILLA <diego.milla@uc.cl>
2023-09-12 00:00:00 -0300

    Working initial

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Routes from './src/Routes'
+
+export default function App () {
+  return (
+    <Routes />
+  )
+}
+/* import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App () {
+  return (
+    <View style={styles.container}>
+      <Text>WoTMobileApp</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+}) */
---
+++ README.md
@@ -0,0 +1,10 @@
+"# 2023-2-S2-Grupo2-Movil" 
+
+## Instrucciones
+
+- Instalar ExpoGo en el celular
+- Instalar Expo en el PC con el comando: `npm i -g expo-cli`
+- Clonar el repositorio
+- Correr el comando: `npm install`
+- Correr el comando: `npx expo start --tunnel`
+- Escanear el código QR con la app de ExpoGo
